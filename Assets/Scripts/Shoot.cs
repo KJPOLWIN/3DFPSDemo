6b9d5ad88f86acdcbfe97f0656543bbb79536233
KJPOLWIN <kjpolwin@gmail.com>
2022-12-26 23:05:23 +0100

    Shooting animations (partially working)

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //gun.transform.localPosition = new Vector3(0, -0.163f, 1.354f);
+            animator.SetTrigger("IdleShoot");
+            animator.ResetTrigger("EndIdleShoot");
+            //Debug.Log("eee");
+        }
+        else
+        {
+            //gun.transform.localPosition = new Vector3(0.5f, -0.25f, 1.5f);
+            animator.ResetTrigger("IdleShoot");
+            animator.SetTrigger("EndIdleShoot");
+        }
+    }
+}
