11d2eb134cbf7076c572ae39da0960cec60ac790
KJPOLWIN <kjpolwin@gmail.com>
2022-12-26 13:04:24 +0100

    Cursor lock, more textures

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -11,7 +11,8 @@ public class Rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
